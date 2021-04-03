54a6b3aaed101d349c165174caf01ff949df803c
AGarciaCast <60430415+AGarciaCast@users.noreply.github.com>
2021-04-03 19:09:38 +0200

    dao categoria

---
+++ Code/java/src/main/java/app/dao/CategoriaDAO.java
@@ -0,0 +1,29 @@
+package app.dao;
+
+
+import app.model.Categoria;
+import app.model.request.plato.section.PlatoRequestCategoria;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class CategoriaDAO extends GenericDAO {
+
+//Query tabla categoria
+  public List<Categoria> getCategorias() throws Exception {
+	    List<Categoria> categorias = new ArrayList<>();
+	    String query = "SELECT * FROM categoria;";
+
+	    System.out.println(connector==null);
+	    try (Connection conn = connector.getConnection()) {
+			categorias =
+	        queryRunner.query(conn, query, new BeanListHandler<>(Categoria.class));
+	    }
+
+	    return categorias;
+	  }
+}
